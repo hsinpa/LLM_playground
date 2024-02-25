3039494da0dc1d23a312c2dc80852b4be4b63272
HSINPA\hsinp <hsinpa@gmail.com>
2024-02-26 00:20:13 +0800

    batching

--- BuildGPU_Andrej/gpt_main.py
+++ BuildGPU_Andrej/gpt_main.py
@@ -1,14 +1,21 @@
+import torch
+
 from BuildGPU_Andrej.character_tokenizer import CharacterTokenizer
-from BuildGPU_Andrej.small_helper import read_full_text
+from BuildGPU_Andrej.small_helper import read_full_text, get_batch
 
 text_file = "./assets/tiny_shakespeare.txt"
 raw_text = read_full_text(text_file)
 
 tokenizer = CharacterTokenizer(raw_text)
 
-encode = tokenizer.encode("hello world")
-decode = tokenizer.decode(encode)
+data = torch.tensor(tokenizer.encode(raw_text), dtype=torch.long)
 
-print(encode)
-print(decode)
+n = int(0.9 * len(data))
+train_data = data[:n]
+val_data = data[n:]
 
+block_size = 8
+batch_size = 4
+
+xb, yb = get_batch(train_data, block_size=block_size, batch_size=batch_size)
+print(xb)
--- BuildGPU_Andrej/small_helper.py
+++ BuildGPU_Andrej/small_helper.py
@@ -1,4 +1,14 @@
+import torch
+
+
 def read_full_text(path: str):
 
     with open(path, "r", encoding="utf-8") as f:
         return f.read()
+
+
+def get_batch(dataset: torch.tensor, block_size: int, batch_size: int):
+    ix = torch.randint(len(dataset) - block_size, (batch_size,))
+    x = torch.stack([dataset[i : i + block_size] for i in ix])
+    y = torch.stack([dataset[i + 1 : i + block_size + 1] for i in ix])
+    return x, y
