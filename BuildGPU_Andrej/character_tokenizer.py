b18211abaef52bfef2837388e64861fd3314d1c0
Hsinpa <hsinpa@gmail.com>
2024-04-16 10:46:05 +0800

    Tokenizer and data loader

--- BuildGPU_Andrej/character_tokenizer.py
+++ BuildGPU_Andrej/character_tokenizer.py
@@ -3,8 +3,10 @@ import re
 class CharacterTokenizer:
 
     def __init__(self, full_text: str, extension: list[str]):
-        self._full_text = full_text
-        self._unique_chars = sorted(list(set(full_text)))
+        preprocessed = re.split(r'([,.?_!"()\']|--|\s)', full_text)
+        preprocessed = [item.strip() for item in preprocessed if item.strip()]
+
+        self._unique_chars = sorted(list(set(preprocessed)))
         self._unique_chars.extend(extension)
 
         self.vocab_lens = len(self._unique_chars)
@@ -13,7 +15,12 @@ class CharacterTokenizer:
         self.__integer_to_string = {i: ch for i, ch in enumerate(self._unique_chars)}
 
     def encode(self, target: str):
-        return [self.__string_to_integer[c] for c in target]
+        preprocessed = re.split(r'([,.?_!"()\']|--|\s)', target)
+        preprocessed = [item.strip() for item in preprocessed if item.strip()]
+        preprocessed = [item if item in self.__string_to_integer else "<|unk|>" for item in preprocessed]
+        ids = [self.__string_to_integer[s] for s in preprocessed]
+
+        return ids
 
     def decode(self, tokens: list[int]):
         text = " ".join([self.__integer_to_string[i] for i in tokens])
--- BuildGPU_Andrej/gpt_chapter_2.py
+++ BuildGPU_Andrej/gpt_chapter_2.py
@@ -1,4 +1,5 @@
 import re
+import tiktoken
 
 from BuildGPU_Andrej.character_tokenizer import CharacterTokenizer
 
@@ -6,4 +7,7 @@ with open("../assets/the-verdict.txt", "r", encoding="utf-8") as f:
     raw_text = f.read()
     print("Total number of character:", len(raw_text))
 
-custom_tokenizer = CharacterTokenizer(raw_text, ["<|endoftext|>", "<|unk|>"])
+tokenizer = tiktoken.get_encoding("cl100k_base")
+
+encode_ids = tokenizer.encode(raw_text)
+enc_sample = encode_ids[50:]
