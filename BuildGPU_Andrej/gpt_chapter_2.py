41f3ff00cfe3358aeed67a3402782f3cf84ab6b5
Hsinpa <hsinpa@gmail.com>
2024-04-16 16:53:27 +0800

    Chapter 3

--- BuildGPU_Andrej/gpt_chapter_2.py
+++ BuildGPU_Andrej/gpt_chapter_2.py
@@ -1,7 +1,7 @@
-import re
 import tiktoken
+import torch
 
-from BuildGPU_Andrej.character_tokenizer import CharacterTokenizer
+from BuildGPU_Andrej.GPTDataset import create_dataloader
 
 with open("../assets/the-verdict.txt", "r", encoding="utf-8") as f:
     raw_text = f.read()
@@ -9,5 +9,21 @@ with open("../assets/the-verdict.txt", "r", encoding="utf-8") as f:
 
 tokenizer = tiktoken.get_encoding("cl100k_base")
 
-encode_ids = tokenizer.encode(raw_text)
-enc_sample = encode_ids[50:]
+max_length = 4
+dataloader = create_dataloader(raw_text, tokenizer, batch_size=8, max_length=max_length, stride=max_length, shuffle=False)
+data_iter = iter(dataloader)
+inputs, targets = next(data_iter)
+print("Token IDs:\n", inputs)
+print("\nInputs shape:\n", inputs.shape)
+
+out_dim = 256
+vocab_size = tokenizer.n_vocab
+token_embedding_layer = torch.nn.Embedding(vocab_size, out_dim)
+token_embeddings = token_embedding_layer(inputs)
+
+context_length = max_length
+pos_embedding_layer = torch.nn.Embedding(context_length, out_dim)
+pos_embeddings = pos_embedding_layer(torch.arange(context_length))
+
+input_embeddings = token_embeddings + pos_embeddings
+print(input_embeddings.shape)
